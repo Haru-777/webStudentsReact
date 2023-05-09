fdc01a5143fa3fe9e2cb8530b14b8bd5268d1eb3
Haru-777 <linahoyos14@gmail.com>
2023-05-08 21:15:59 -0500

    fixes login and register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,8 @@
 import React, { useRef } from "react";
 import '../styles/register.scss';
 import '../styles/global.scss';
-import logo from '../assets/logos/logoSinFondo.png';
-import txtlogo from '../assets/logos/smartFCLogo.png';
+import reglogo from '../assets/logos/regssma.png';
+import txtreg from '../assets/logos/regtxt.png'
 
 const Login = () => {
     const form = useRef(null);
@@ -18,33 +18,37 @@ const Login = () => {
     }
 
     return (
-        <div className="Register">
+        <div className="Bottom">
             <div className="Register-container">
-                <img src={logo} alt="logo" className="logo" ></img>
-                <img src={txtlogo} alt="logo" className="logotxt" />
-                <form action="/" className="form" ref={form}>
-                    <div className="inputContainer">
+                <img src={reglogo} alt="logo" className="reg-logo" ></img>
+                <form action="/" className="form-ref" ref={form}>
+                    <div className="inp-name">
                         <input type="text" name="fname" placeholder="Nombres" className="inp-nametxt" required />
-                        <label htmlFor="name" className="label" >Nombres</label></div>
-                    <div className="inputContainer">
-                        <input type="text" name="lname" placeholder="Apellidos" className="inp-lastnametxt" required />
-                        <label htmlFor="apellidos" className="label" >Apellidos</label></div>
-                    <div className="inputContainer">
-                        <label htmlFor="school" className="label" >Seleccione su colegio:</label>
-
-                        <select name="choseschool" id="choseSchool">
+                        <label htmlFor="name" className="name-lab" >Nombres</label>
+                    </div>
+                    <div className="inp-lname">
+                        <input type="text" name="lname" placeholder="Apellidos" className="inp-lnametxt" required />
+                        <label htmlFor="apellidos" className="lname-lab" >Apellidos</label></div>
+                    <div className="inp-sch">
+                        <label htmlFor="school" className="sch-lab" >Seleccione su colegio:</label>
+                        <select className="sch-ch" id="choseSchool">
                             <option value="volvo">Colegio 1</option>
                             <option value="saab">Colegio 2</option>
                             <option value="mercedes">Colegio 1</option>
                         </select>
+                    </div>
+                    <div>
                         <input type="email" name="email" placeholder="Correo Electronico" className="inp-emailtxt" required />
-                        <label htmlFor="email" className="label" >Correo Electronico</label></div>
+                        <label htmlFor="email" className="label" >Correo Electronico</label>
+                    </div>
                     <div className="inputContainer">
                         <input type="password" name="password" placeholder="Contraseña" className="inp-passtxt" required />
-                        <label htmlFor="password" className="labelcx">Contraseña</label></div>
+                        <label htmlFor="password" className="labelcx">Contraseña</label>
+                    </div>
                     <div className="inputContainer">
                         <input type="password" name="password" placeholder="Contraseña" className="inp-passtxt" required />
-                        <label htmlFor="password" className="labelcx">Contraseña</label></div>
+                        <label htmlFor="password" className="labelcx">Contraseña</label>
+                    </div>
                     <button
                         onClick={handleSubmit}
                         className="btnlogin">
