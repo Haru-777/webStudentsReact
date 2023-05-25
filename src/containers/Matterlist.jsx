b86d8b02609e4b8da98ab6f2824d4fbe1db9af57
Haru-777 <linahoyos14@gmail.com>
2023-05-24 19:19:03 -0500

    fix: modal activity

--- src/containers/Matterlist.jsx
+++ src/containers/Matterlist.jsx
@@ -5,17 +5,19 @@ import '../styles/global.scss';
 import Modal from '../modals/Modal';
 import Search from '../components/Search';
 import Information from '../components/Information';
+import InfCou from '../modals/InfCou';
 
 const Matterlist = () => {
 
-	const [openModal, setOpenModal] = useState(false)
+	const [openModal, setOpenModal] = useState(false);
 
 	return (
 		<section className="main-container">
 			<div className='Bottom'>
 				<div className='search-sh'>
 					<Search/>
-					<Information/>
+					<Information onClick={() => setOpenModal(true)}/>
+					<InfCou open={openModal} onClose={()=>setOpenModal(false)}/>
 				</div>
 				<ul className='matterList'>
 					<li>
