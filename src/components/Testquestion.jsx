6a343fddbab068f82657421ac19e1d85418fe91c
Haru-777 <linahoyos14@gmail.com>
2023-06-01 10:33:31 -0500

    FEAT: test

--- src/components/Testquestion.jsx
+++ src/components/Testquestion.jsx
@@ -6,14 +6,41 @@ const Testquestion = () => {
     const [acQuestion, setacQuestion] = useState(0);
     const [puntuacion, setpuntuacion] = useState(0);
     const [isFinish, setisFinish] = useState(false);
+    const [restTime, setrestTime] = useState(10);
+    const [areDisable, setareDisable] = useState(false);
 
-    function handleAnswSubmit(isCorrect, e){
+    function handleAnswSubmit(isCorrect, e) {
         //puntuacion
-        if(isCorrect) setpuntuacion(puntuacion + 1);
+        if (isCorrect) setpuntuacion(puntuacion + 1);
         //estilos
-        e.target.classList.add(isCorrect ? "correct" : "incorrect")
+        e.target.classList.add(isCorrect ? "correct" : "incorrect");
+        //cambiar a la sg pregunta
 
+        setTimeout(() => {
+            if (acQuestion === preguntas.length - 1) {
+                setisFinish(true);
+            }
+            else {
+                setacQuestion(acQuestion + 1);
+
+            }
+        }, 1000);
     }
+
+    useEffect(() => {
+        const intervalo = setInterval(() => {
+            if (restTime > 0) setrestTime((prev) => prev - 1);
+            if (restTime === 0) setareDisable(true);
+        }, 1000);
+        return () => clearInterval(intervalo);
+    }, [restTime]);
+
+    if (isFinish) return (
+        <main className='test-container'>
+            <div className='up-cont'></div>
+            <span> Obtuviste {puntuacion} de {preguntas.length}</span>
+        </main>
+    )
 
 
     return (
@@ -28,12 +55,30 @@ const Testquestion = () => {
             </div>
             <div className='down-cont'>
                 {preguntas[acQuestion].opciones.map((respuesta) => (
-                    <button 
-                    key={respuesta.textoRespuesta} 
-                    onClick={(e) => handleAnswSubmit(respuesta.isCorrect, e)}>
+                    <button
+                        disabled={areDisable}
+                        key={respuesta.textoRespuesta}
+                        onClick={(e) => handleAnswSubmit(respuesta.isCorrect, e)
+                        }>
                         {respuesta.textoRespuesta}
                     </button>
                 ))}
+                <div>
+                    {!areDisable ? (
+                        <span className='rest-time'>Tiempo restante: {restTime} </span>
+                    ) : (
+                        <div className='dt'>
+                        <p className='txt-t'>Se ha terminado tu tiempo, porfavor da click en continuar.</p>
+                        <button className='ctn-btn'
+                            onClick={() => {
+                            setrestTime (10);
+                            setareDisable(false);
+                            setacQuestion(acQuestion + 1 ); }}>
+                            Continuar
+                        </button>
+                        </div>
+                    )}
+                </div>
 
             </div>
         </div>
